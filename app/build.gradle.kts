ee320fa2074ec28ae81ad74cd56605119fc29fb9
hathiep <havanthiep2308@gmail.com>
2024-07-19 02:58:46 +0700

    Update Activity CreatePost - Road Step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.core:core-animation:1.0.0")
 }
--- app/src/main/java/com/example/nhatro360/CreatePost.java
+++ app/src/main/java/com/example/nhatro360/CreatePost.java
@@ -1,10 +1,12 @@
 package com.example.nhatro360;
 
-import android.content.DialogInterface;
+import android.animation.ObjectAnimator;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowInsetsController;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -15,10 +17,17 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import android.os.Handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CreatePost extends AppCompatActivity {
 
     private TextView tvCancel, tvNext;
+    private List<TextView> listTv;
+    private List<ImageView> listImv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +62,7 @@ public class CreatePost extends AppCompatActivity {
             return insets;
         });
 
-        tvCancel = findViewById(R.id.tv_cancel);
-        tvNext = findViewById(R.id.tv_next);
+        init();
 
         if (savedInstanceState == null) {
             loadFragment(new FragmentAddress(), false);
@@ -70,16 +78,34 @@ public class CreatePost extends AppCompatActivity {
         updateButtons();
     }
 
+    private void init() {
+        listImv = new ArrayList<>();
+        listTv = new ArrayList<>();
+        tvCancel = findViewById(R.id.tv_cancel);
+        tvNext = findViewById(R.id.tv_next);
+        listTv.add(findViewById(R.id.tv_address));
+        listTv.add(findViewById(R.id.tv_information));
+        listTv.add(findViewById(R.id.tv_image));
+        listTv.add(findViewById(R.id.tv_confirm));
+        listImv.add(findViewById(R.id.imv_address));
+        listImv.add(findViewById(R.id.imv_information));
+        listImv.add(findViewById(R.id.imv_image));
+        listImv.add(findViewById(R.id.imv_confirm));
+    }
+
     private void setOnclickHeader() {
 
         tvNext.setOnClickListener(v -> {
             Fragment currentFragment = getCurrentFragment();
             if (currentFragment instanceof FragmentAddress) {
                 loadFragment(new FragmentInformation(), true);
+                setNextStep(1, 0);
             } else if (currentFragment instanceof FragmentInformation) {
                 loadFragment(new FragmentImage(), true);
+                setNextStep(2, 1);
             } else if (currentFragment instanceof FragmentImage) {
                 loadFragment(new FragmentConfirm(), true);
+                setNextStep(3,2);
             }
         });
 
@@ -88,6 +114,15 @@ public class CreatePost extends AppCompatActivity {
             if (currentFragment instanceof FragmentAddress) {
                 showCancelDialog();
             } else {
+                if(currentFragment instanceof FragmentConfirm) {
+                    setBackStep(2, 3);
+                }
+                else if(currentFragment instanceof FragmentImage) {
+                    setBackStep(1, 2);
+                }
+                else if(currentFragment instanceof FragmentInformation) {
+                    setBackStep(0, 1);
+                }
                 getSupportFragmentManager().popBackStack();
             }
         });
@@ -112,6 +147,51 @@ public class CreatePost extends AppCompatActivity {
         updateButtons(); // Cập nhật nút sau khi fragment thay đổi
     }
 
+    private void setNextStep(int current, int complete) {
+        listTv.get(complete).setTextColor(getResources().getColor(R.color.blue2));
+        setAnimation(complete, 1, 1);
+        new Handler().postDelayed(() -> setAnimation(current, 2, 1), 200);
+    }
+
+    private void setBackStep(int current, int inComplete) {
+        listTv.get(current).setTextColor(getResources().getColor(R.color.black2));
+        setAnimation(inComplete, 3, 2);
+        new Handler().postDelayed(() -> setAnimation(current, 2, 2), 100);
+    }
+    private void setAnimation(int cnt, int i, int direction){
+        int time_delay = 100;
+        ImageView imv = listImv.get(cnt);
+        float scaleA, scaleB;
+        if(direction == 1) {
+            scaleA = 0f;
+            scaleB = 1f;
+            listImv.get(cnt).setImageResource(0);
+            if(i == 1) imv.setImageResource(R.drawable.icon_completed_step);
+            else imv.setImageResource(R.drawable.icon_current_step);
+        } else {
+            scaleA = 1f;
+            scaleB = 0f;
+            if(i == 3) {
+                listImv.get(cnt).setImageResource(0);
+                imv.setImageResource(R.drawable.icon_current_step);
+            }
+            else {
+                imv.setImageResource(R.drawable.icon_completed_step);
+            }
+        }
+        imv.setScaleX(scaleA);
+        imv.setScaleY(scaleA);
+        new Handler().postDelayed(() -> {
+            ObjectAnimator scaleX = ObjectAnimator.ofFloat(imv, "scaleX", scaleA, scaleB);
+            ObjectAnimator scaleY = ObjectAnimator.ofFloat(imv, "scaleY", scaleA, scaleB);
+            scaleX.setDuration(100); // Thời gian hiệu ứng (1 giây)
+            scaleY.setDuration(100);
+            scaleX.start();
+            scaleY.start();
+        }, time_delay);
+        if(direction == 2 && i == 2) new Handler().postDelayed(() ->  listImv.get(cnt).setImageResource(R.drawable.icon_current_step), 1000);
+    }
+
     private void showCancelDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this)
                 .setMessage(R.string.cancel_post_message)
@@ -133,7 +213,6 @@ public class CreatePost extends AppCompatActivity {
         dialog.show();
     }
 
-
     private void updateButtons() {
         if (tvCancel == null || tvNext == null) {
             return; // Tránh lỗi NullPointerException
@@ -149,7 +228,7 @@ public class CreatePost extends AppCompatActivity {
         }
 
         if (currentFragment instanceof FragmentConfirm) {
-            tvNext.setText(R.string.confirm);
+            tvNext.setText(R.string.post);
         } else {
             tvNext.setText(R.string.next);
         }
